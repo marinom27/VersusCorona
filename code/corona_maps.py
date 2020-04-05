385fc37f1006f751e3bb56dcc7948c502f89ea5c
Walya96 <yan130@hotmail.de>
2020-04-05 11:53:15 +0200

    endlosschleife

--- code/corona_maps.py
+++ code/corona_maps.py
@@ -97,72 +97,76 @@ for i in range(len(endstations)):
 
 
 #get context
-
-start = input("From? ")
-destination = input("To? ")
-h = int(input("Hour? "))
-m = int(input("Minute "))
-timebefore = int(input("Time flexibility in mins? "))
-"""
-hour =11
-minute = 15
-timebefore = 120 #time free before arrival time (in mins)
-start = "Zehntenhausplatz,Zürich"
-destination = "ETH Zürich"
-"""
-dt = datetime.now().replace(hour=h+2,minute=m,day=6) #monday
-
-now = datetime.now()
-
-routes = []
-
-for i in range (0,timebefore,30):
-
-
-    route= dir_arrtime(start,destination,dt-timedelta(minutes=i))   #dir_arrtime for arrivaltime; dir_deptime for deptime
-    for j in range(0,len(route)):
-        r=[parse_overral(route[j]),parse_steps(route[j]),0] #[overall route infos,steps infos,rating]
-        #pprint(r)
-        if r not in routes:
-            routes.append(r)
-#pprint(routes)
-print()
-print("Calculating ",len(routes), "possible routes...")
-print()
-for r in range(0,len(routes)):
-    ratio=0
-    count=0
-    for j in range(len(routes[r][1])):
-
-        if(routes[r][1][j].get("type")=="TRANSIT"):
-            count+=1
-            dep = process.extractOne(routes[r][1][j].get("dep"),halteList)[0]
-            dep_time=routes[r][1][j].get("dep_time")
-            towards = routes[r][1][j].get("towards")
-            line = routes[r][1][j].get("line")
-
-            dir = dirs.get(process.extractOne(towards,halteList)[0])
-            """
-            print(dep,dep_time,"Line: ",line, "towards: ",dir)
-            #pred_dep =predict_besetzung(dep_time, line, dep, dir)
-            ratio_dep =1#capacities.get(32).get("overall")
-            #print(ratio_dep)
-
-            arr = process.extractOne(routes[r][1][j].get("arr"),halteList)[0]
-            arr_time=routes[r][1][j].get("arr_time")
-            print(arr,arr_time)
-
-            #pred_arr = predict_besetzung(arr_time, line, arr, dir)
-            ratio_arr = 1# pred_dep[0]/capacities.get(32).get("overall")
-            #print(ratio_arr)
-            """
-
-
-
-            ratio+=(ratio_dep+ratio_arr)
-
-    if(count!=0):
-        ratio/=count*2
-        routes[r][2]=ratio
-        print(ratio)
+finished = False
+while(not finished):
+    start = input("From? ")
+    destination = input("To? ")
+    h = int(input("Hour? "))
+    m = int(input("Minute "))
+    timebefore = int(input("Time flexibility in mins? "))
+    """
+    hour =11
+    minute = 15
+    timebefore = 120 #time free before arrival time (in mins)
+    start = "Zehntenhausplatz,Zürich"
+    destination = "ETH Zürich"
+    """
+    dt = datetime.now().replace(hour=h+2,minute=m,day=6) #monday
+    
+    now = datetime.now()
+    
+    routes = []
+    
+    for i in range (0,timebefore,30):
+        
+        
+        route= dir_arrtime(start,destination,dt-timedelta(minutes=i))   #dir_arrtime for arrivaltime; dir_deptime for deptime
+        for j in range(0,len(route)):
+            r=[parse_overral(route[j]),parse_steps(route[j]),0] #[overall route infos,steps infos,rating]
+            #pprint(r)
+            if r not in routes:
+                routes.append(r)
+                #pprint(routes)
     print()
+    print("Calculating ",len(routes), "possible routes...")
+    print()
+    for r in range(0,len(routes)):
+        ratio=0
+        count=0
+        for j in range(len(routes[r][1])):
+
+            if(routes[r][1][j].get("type")=="TRANSIT"):
+                count+=1
+                dep = process.extractOne(routes[r][1][j].get("dep"),halteList)[0]
+                dep_time=routes[r][1][j].get("dep_time")
+                towards = routes[r][1][j].get("towards")
+                line = routes[r][1][j].get("line")
+                
+                dir = dirs.get(process.extractOne(towards,halteList)[0])
+                """
+                print(dep,dep_time,"Line: ",line, "towards: ",dir)
+                #pred_dep =predict_besetzung(dep_time, line, dep, dir)
+                ratio_dep =1#capacities.get(32).get("overall")
+                #print(ratio_dep)
+                
+                arr = process.extractOne(routes[r][1][j].get("arr"),halteList)[0]
+                arr_time=routes[r][1][j].get("arr_time")
+                print(arr,arr_time)
+                
+                #pred_arr = predict_besetzung(arr_time, line, arr, dir)
+                ratio_arr = 1# pred_dep[0]/capacities.get(32).get("overall")
+                #print(ratio_arr)
+                """
+
+
+
+                #ratio+=(ratio_dep+ratio_arr)
+                
+        if(count!=0):
+            ratio/=count*2
+            routes[r][2]=ratio
+            print(ratio)
+        print()
+    inp = input("do you want to start another request?y/n ")
+    if (inp == "n"):
+        finished = True
